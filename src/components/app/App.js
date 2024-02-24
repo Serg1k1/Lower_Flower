d473234e27bea90cf5bedcf269088ab68a84bdf4
Serhii <kindratsergij60@gmail.com>
2024-02-24 13:26:27 +0200

    init: setup main dependencies and styles

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import Header from "../header/Header";
+
+
+const App = () => {
+    return (
+        <div className="wrapper">
+            <Header />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <h1>Test</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
